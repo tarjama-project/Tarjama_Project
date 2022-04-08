3ac88daa8fe2582b68ef2b7de034257732f97f62
emanmkhareez <emkhareez19@gmail.com>
2022-04-08 22:26:41 +0300

    Finallly ❤❤🌹🌹🌹❤❤🌹

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -10,7 +10,6 @@ import Swal from 'sweetalert2';
 export default function Posts() {
 
     const posts = useSelector(state => state.postsState);
-    console.log("posttt",posts)
     const myInfos = useSelector(state => state.userInfoState);
     const dispatch = useDispatch();
 
@@ -24,7 +23,7 @@ export default function Posts() {
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
             confirmButtonText: 'Yes, delete it!'
-        }).then(async(result) => {
+        }).then(async (result) => {
             if (result.isConfirmed) {
 
                 let newPosts = await functions_Handeler.deletePostFun(posts, idx);
@@ -86,28 +85,43 @@ export default function Posts() {
             <h1 className="text-center">Posts</h1>
 
             <div>
+
+                <div class="m-auto w-full max-w-xs">
                 <h3> Add New Post </h3>
-
-                <Form onSubmit={addPostHandler}>
-                    <FormGroup>
-                        <Form.Label>Title: </Form.Label>
-                        <Form.Control as="textarea" placeholder='Title ...' name="title" type="text" />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Form.Label>Body: </Form.Label>
-                        <Form.Control as="textarea" placeholder='Post Body ...' name="body" type="text" />
-                    </FormGroup>
-
-                    <Button type="submit" variant="primary">
+                <form onSubmit={addPostHandler} class="bg-gray shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        Title:
+                        </label>
+                        <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required  placeholder='Title ...' name="title" type="text" />
+                    </div>
+                    <div class="mb-6">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                        Body:
+                        </label>
+                        <textarea class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" required placeholder='Post Body ...' name="body" type="text" />
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                         Add
-                    </Button>
-                </Form>
+                        </button>
+
+                    </div>
+                </form>
+
+            </div>
+
+                
             </div>
             <div class="grid grid-cols-3 gap-4 place-items-start  ..." >
-            {
+                {
+                    posts.length == 0 &&
+                    <div className="fixed top-0 left-0 right-0 bottom-0 z-30 bg-black opacity-40 ">
+                        <img className="w-2/4 m-auto " alt="loading" src="https://cdn.dribbble.com/users/2973561/screenshots/5757826/loading__.gif" />
+                    </div>
+                }
+                {
                     posts.map((myPost, idx) => {
-                        console.log(myPost)
                         return (
                             <div key={idx}>
                                 {/* <h2>{myPost.title}</h2>
@@ -122,6 +136,7 @@ export default function Posts() {
                                     </div>
 
                                     <ul className="px-3">
+                                    <p className="bg-gray-300"> Comments: </p>
                                         {
                                             myPost.comments.map((comm, index) => {
                                                 return (
@@ -164,7 +179,7 @@ export default function Posts() {
 
 
                 }
-                </div>
+            </div>
             {/* {
                 posts.map((myPost, idx) => {
                     return (
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../actions/signIn";
-import * as function_Handlers from "../actions/API_Funtions_Handeler"
+import * as actions2 from "../actions/posts";
+import * as actions3 from "../actions/allAlbums";
+import * as function_Handlers from "../actions/API_Funtions_Handeler";
+// import Swal from 'sweetalert2/dist/sweetalert2.js'
+// import 'sweetalert2/src/sweetalert2.scss'
 import Swal from 'sweetalert2'
-import '../index.css'
 
 
 export default function SignIn() {
 
     const logged = useSelector(state => state.isLogged);
+    const allUsers = useSelector(state => state.allUsers);
 
     const dispatch = useDispatch();
 
@@ -25,6 +29,16 @@ export default function SignIn() {
         if (logInResult) {
             dispatch(actions.signIn());
             dispatch(actions.userInfo(logInResult));
+            if (allUsers.length === 0) {
+                let allUsers = await function_Handlers.allUsers();
+                dispatch(actions.allUsers(allUsers));
+
+                let posts = await function_Handlers.getPosts();
+                dispatch(actions2.getPosts(posts));
+
+                let allAlbums = await function_Handlers.allAlbums();
+                dispatch(actions3.allAlbums(allAlbums));
+            }
         } else {
             Swal.fire({
                 icon: 'error',
@@ -34,82 +48,41 @@ export default function SignIn() {
         }
 
     }
+
     return (
-        <div >
+        <div className="text-center">
+            <div>
+            <h2 className="mt-16 mb-16"> Log In Form </h2>
+
+            {/*  */}
+            <div class="m-auto w-full max-w-xs">
+                <form onSubmit={signInFunc} class="bg-gray shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                            Username
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required type="text" placeholder='userName' name="name" id="name" />
+                    </div>
+                    <div class="mb-6">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                            Email
+                        </label>
+                        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" required type="email" placeholder='email' name="email" id="email" />
+                        <p class="text-red-500 text-xs italic">Please choose your email and username.</p>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                            Sign In
+                        </button>
+
+                    </div>
+                </form>
+
+            </div>
+            </div>
             
-            <h2 className=' flex flex-col justify-center items-center;' > Log In Form </h2>
-            {/* <form onSubmit={signInFunc}>
-                <div class="md:flex md:items-center mb-6">
-               
-                    <input required type="text" placeholder='name' name="name" id="name" />
-                    <input required type="email" placeholder='email' name="email" id="email" />
-                    <input type='submit' value="Sign In" />
-
-                </div>
-
-
-            </form> */}
-
-
-
-
-
-
-
-
-
-
-<div >
-            <form class="w-full max-w-sm flex flex-col justify-center items-center;" onSubmit={signInFunc}>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        name
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input  name="name" id="name" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"  type="text"/>
-    </div>
-  </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-        email
-      </label>
-    </div>
-    <div class="md:w-2/3">
-      <input name="email" id="email" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text"/>
-    </div>
-  </div>
-  <div class="md:flex md:items-center mb-6">
-    <div class="md:w-1/3"></div>
-
-  </div>
-  <div class="md:flex md:items-center">
-    <div class="md:w-1/3"></div>
-    <div class="md:w-2/3">
-    <input type='submit' value="Sign In" />
-      
-    </div>
-  </div>
-</form>
-</div>
-
-
-
-
-
-
+            {/*  */}
 
         </div>
-
-
-
-
-
-
-
-
-
     )
 }
